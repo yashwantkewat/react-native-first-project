02978c41fc5d5751197cb133f51016a818b8bfaf
yashwantkewat <yashkewat78@gmail.com>
2025-08-04 12:17:35 +0530

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import {   Text, View } from 'react-native';
+
+
+
+export default function HomeScreen() {
+  return (
+   
+      <View >
+        <Text >Welcome! </Text>
+      </View>
+     
+     
+  );
+}
+
+
